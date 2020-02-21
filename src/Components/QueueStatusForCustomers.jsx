fc782f98a5f698ab93afa06c8c220afdae776526
luigutie <luigutierrezperez@bestbuycanada.ca>
2020-02-21 11:46:54 -0800

    removing unnecesary header

--- src/Components/QueueStatusForCustomers.jsx
+++ src/Components/QueueStatusForCustomers.jsx
@@ -43,8 +43,6 @@ export default class QueueStatusForCustomers extends Component {
         return (
             <div className='container-fluid'>
                 <div className='row d-flex justify-content-center mt-5'>
-                    <div className='col-6 text-left pt-4 pl-0'><h4>Customer to queue status</h4></div>
-
                     <div className='col-6 m-5 rounded'>
 
                         <table class="table table-bordered f-18">
